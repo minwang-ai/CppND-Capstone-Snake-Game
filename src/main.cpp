1799c7c95631788e3b23552ddcadc4cd156520e3
Min <professional@minwang-ai.com>
2025-02-02 20:23:12 +0100

    add player

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,24 @@
 #include "controller.h"
 #include "game.h"
 #include "renderer.h"
+#include "player.h"
+
+void CreatePlayer() {
+  std::string name;
+  std::cout << "Please enter your name: ";
+  std::getline(std::cin, name);
+  Player player_(name);
+}
+
+void GameOver(){
+  player.SetScore(game.GetScore());
+
+  std::cout << "Game has terminated successfully!\n";
+  std::cout << "Player: " << player.GetName() << "\n";
+  std::cout << "Score: " << game.GetScore() << "\n";
+  std::cout << "Size: " << game.GetSize() << "\n";
+  std::cout << "Game Duration: " << player.GetGameDuration() << " seconds\n";
+}
 
 int main() {
   constexpr std::size_t kFramesPerSecond{60};
@@ -11,12 +29,13 @@ int main() {
   constexpr std::size_t kGridWidth{32};
   constexpr std::size_t kGridHeight{32};
 
+  CreatePlayer();
+
   Renderer renderer(kScreenWidth, kScreenHeight, kGridWidth, kGridHeight);
   Controller controller;
   Game game(kGridWidth, kGridHeight);
   game.Run(controller, renderer, kMsPerFrame);
-  std::cout << "Game has terminated successfully!\n";
-  std::cout << "Score: " << game.GetScore() << "\n";
-  std::cout << "Size: " << game.GetSize() << "\n";
+
+  GameOver();
   return 0;
 }
---
+++ src/player.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include <chrono>
+
+class Player {
+public:
+    // constructor
+    Player(std::string name)
+        : name_(name), score_(0), start_time_(std::chrono::system_clock::now()) {}
+    // setters and getters
+    void SetScore(int score) { 
+        score_ = score; 
+        end_time_ = std::chrono::system_clock::now(); 
+    }
+
+    const std::string& GetName() const { return name_; }
+    int GetScore() const { return score_; }
+    std::chrono::system_clock::time_point GetStartTime() const { return start_time_; } 
+    std::chrono::system_clock::time_point GetEndTime() const { return end_time_; }
+
+    double GetGameDuration() const {
+        return std::chrono::duration<double>(end_time_ - start_time_).count();
+    }
+
+private:
+    std::string name_;
+    int score_;
+    const std::chrono::system_clock::time_point start_time_;
+    std::chrono::system_clock::time_point end_time_;
+};
